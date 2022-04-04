706f231f8d84a6da3bfd76846c221f5874d226d6
KIM, JINWOOK <zelonion@gmail.com>
2022-04-04 23:42:03 +0900

    HomeView에서 한글 번역 적용

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <hello-world />
+  <p v-html="t('message')"></p>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+// https://velog.io/@latte_h/Vue3-Guide-17-Vue-3%EC%97%90%EC%84%9C-%EB%8B%A4%EA%B5%AD%EC%96%B4-%EC%B2%98%EB%A6%AC
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
   },
+  setup() {
+    const { t } = useI18n({
+      inheritLocale: true,
+      useScope: 'local'
+    })
+
+    // Something todo ..
+
+    return { t }
+  }
 });
 </script>
