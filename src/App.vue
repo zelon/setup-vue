53fbd16c3830ba65a9cabbf74217b3a9b7052f51
KIM, JINWOOK <zelonion@gmail.com>
2022-04-04 23:12:13 +0900

    apply sub list

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
   <v-app>
-      <v-navigation-drawer permanent>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="text-h6">
-              Application
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              subtext
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+      <v-navigation-drawer permanent width="400">
+      <v-list v-model:opened="open">
+        <v-list-item prepend-icon="mdi-home" title="Home"></v-list-item>
   
-        <v-divider></v-divider>
+        <v-list-group>
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-account-circle" title="Users" value="Users"></v-list-item>
+          </template>
   
-        <v-list dense nav>
-          <v-list-item v-for="item in items" :key="item.title" link>
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
+          <v-list-group>
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" title="Admin" value="Admin"></v-list-item>
+            </template>
   
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+            <v-list-item
+              v-for="([title, icon], i) in admins"
+              :key="i"
+              :value="title"
+              :title="title"
+              :prepend-icon="icon"
+            ></v-list-item>
+          </v-list-group>
+  
+          <v-list-group>
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" title="Actions" value="Users"></v-list-item>
+            </template>
+  
+            <v-list-item
+              v-for="([title, icon], i) in cruds"
+              :key="i"
+              :value="title"
+              :title="title"
+              :prepend-icon="icon"
+            ></v-list-item>
+          </v-list-group>
+        </v-list-group>
+      </v-list>
+
       </v-navigation-drawer>
     <v-main>
       <v-btn v-for="item in zelons" :key="item">{{ item }}</v-btn>
@@ -48,6 +62,17 @@ export default defineComponent({
         { title: 'About', icon: 'mdi-help-box' },
       ],
       right: null,
+      open: ['Users'],
+    admins: [
+      ['Management', 'mdi-account-multiple-outline'],
+      ['Settings', 'mdi-cog-outline'],
+    ],
+    cruds: [
+      ['Create', 'mdi-plus-outline'],
+      ['Read', 'mdi-file-outline'],
+      ['Update', 'mdi-update'],
+      ['Delete', 'mdi-delete'],
+    ],
     }
   },
 })
